e426ea2b7813a62688f878e5c17483d890cbd484
dbartworks <contact@dbartworks.fr>
2024-07-16 07:49:50 +0100

    routes.ts

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,10 +102,6 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      "Uncomment this file and remove this line. You can delete this file when you are finished.",
-  });
   try {
     await client.sql`BEGIN`;
     await seedUsers();
